1a20b80d599481eaae814b6aa755e548ceaf0dee
Sathish Ramasami <sathishramasamy2019@gmail.com>
2022-12-14 03:28:04 -0800

    home page

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
+          <Route path="test-react" index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<Nopage />} />
